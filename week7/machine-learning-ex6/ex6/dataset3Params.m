4819ca57d2f3a403745bfc45b58b5007362e1a2b
Gustavo Soares Souza <gustavosoares@gmail.com>
2015-09-27 18:05:08 -0300

    second exercise.. week 7)

--- week7/machine-learning-ex6/ex6/dataset3Params.m
+++ week7/machine-learning-ex6/ex6/dataset3Params.m
@@ -23,10 +23,27 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+C_values = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+sigma_values = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
 
-
-
-
+m = size(C_values, 1);
+min_error = 1000000000;
+for c = 1:m,
+  C_ = C_values(c);
+  for s = 1:m,
+    sigma_ = sigma_values(s);
+    printf ("Training model with C=%.2f and sigma=%.2f \n",
+        C_, sigma_);
+    model= svmTrain(X, y, C_, @(x1, x2) gaussianKernel(x1, x2, sigma_));
+    predictions = svmPredict(model, Xval);
+    error = mean(double(predictions ~= yval));
+    if error < min_error,
+      C = C_;
+      sigma = sigma_;
+      min_error = error;
+    end
+  end;
+end;
 
 
 % =========================================================================
